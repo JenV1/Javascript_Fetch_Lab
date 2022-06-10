80cb291a1b546038fa6a48bd5659a3ecf5c55c21
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-06-10 09:51:09 +0100

    form is at the top of page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 console.log("HI!")
 
 const header = document.querySelector("#loading");
-
+const section1Para = document.querySelector("#content");
+const description = document.querySelector("#description");
+const flag = document.querySelector("#flag");
 
 function getCountryByName(countryName) {
-    const section1 = document.querySelector("#body__firstSection");
-    const newObject = document.createElement("p");
+    // const section1 = document.querySelector("#body__firstSection");
+    // const newObject = document.createElement("p");
     const fetchCountry = fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     fetchCountry.then(response => {
         if (response.ok) {
@@ -16,34 +18,37 @@ function getCountryByName(countryName) {
         throw new Error("Couldn't find country " + countryName);
     })
     .then(data => {
-        newObject.innerHTML = data[0].name.common;
-        newObject.innerHTML += "<br></br>" + "Population: " + data[0].population;
+        section1Para.innerHTML ="";
+        description.innerHTML = data[0].name.common;
+        description.innerHTML += "<br></br>" + "Population: " + data[0].population;
+        flag.innerHTML = data[0].flag;
         console.log(data);
         // wait(1000);
         header.innerHTML = "Loaded.";
     }) 
-    section1.appendChild(newObject);
+    // section1.appendChild(newObject);
 }
 
 
 function getAllCountries() {
-    const section1 = document.querySelector("#body__firstSection");
-    const newObject = document.createElement("p");
+    // const section1 = document.querySelector("#body__firstSection");
+    // const newObject = document.createElement("p");
     const fetchCountry = fetch(`https://restcountries.com/v3.1/all`);
     fetchCountry.then(response => {
+        header.innerHTML = "Loading...";
         return response.json();
     })
     .then(data => {
+        section1Para.innerHTML = "";
         data.forEach(country => {
-            newObject.innerHTML += country.name.common;
-            newObject.innerHTML += "<br></br>" + "Population: " + country.population + "<br></br><br></br>";
+            section1Para.innerHTML += country.name.common;
+            section1Para.innerHTML += "<br></br>" + "Population: " + country.population + "<br></br><br></br>";
             console.log(country);
+            header.innerHTML = "Loaded.";
         })
     }) 
-    section1.appendChild(newObject);
+    // section1.appendChild(newObject);
 }
-
-
 
 const form = document.querySelector("#form");
 
@@ -63,4 +68,5 @@ function wait(ms){
 
 document.addEventListener('DOMContentLoaded', () => {
     header.innerHTML = "Loaded.";
+    getAllCountries();
 });
